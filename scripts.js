cc392f80f6a2cf8c3b99dbb8e278ce11dc7d7580
Wep <eetu.palviainen@gmail.com>
2024-10-03 14:50:24 +0300

    Add operate function that handles different calculations

--- scripts.js
+++ scripts.js
@@ -1,3 +1,29 @@
+
+
+let number1 = 0;
+let operator = "";
+let number2 = 0;
+
+function operate(number1, number2, operator) {
+
+    switch(operator) {
+        case "+" :
+            add(number1, number2);
+            break;
+        case "-" :
+            subtract(number1, number2);
+            break;
+        case "/" :
+            divide(number1 / number2);
+            break;
+        case "*" :
+            multiply(number1, number2);
+        default :
+            alert("Something went wrong...")
+    }
+}
+
+
 function add(...args) {
     return args.reduce((sum, arg) => sum + arg, 0);
     }
