2259d84c8faa509c4a28edf4a309dfabe70ada69
Wep <eetu.palviainen@gmail.com>
2024-10-03 14:46:47 +0300

    Add functions for adding, dividing, multiplying and dividing

--- scripts.js
+++ scripts.js
@@ -1,2 +1,16 @@
-
+function add(...args) {
+    return args.reduce((sum, arg) => sum + arg, 0);
+    }
+    
+    function subtract (...args) {
+    return args.reduce((total, arg) => total - arg, args[0]);
+    }
+    
+    function multiply (...args) {
+    return args.reduce((total, arg) => total * arg, 1);
+    }
+    
+    function divide(...args) {
+    return args.reduce((total, args) => total / args);
+    }
 
